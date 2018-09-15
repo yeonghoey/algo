ab35ef0f1d215095f6c1cbe9b15fa902cef3d6b0
Yeongho Kim <yeonghoey@gmail.com>
2018-09-15 19:23:28 +0900

    Implement digits.add

--- karatsuba/digits.go
+++ karatsuba/digits.go
@@ -2,11 +2,11 @@ package karatsuba
 
 import "fmt"
 
-type digits = []int8
+type digits []int8
 
 func toDigits(s string) (digits, error) {
 	n := len(s)
-	ds := make([]int8, n)
+	ds := make(digits, n)
 
 	for i, c := range s {
 		d := int8(c - '0')
@@ -19,6 +19,58 @@ func toDigits(s string) (digits, error) {
 	return ds, nil
 }
 
-func isEqual(a, b digits) bool {
+func (ds digits) isEqual(other digits) bool {
+	if (ds == nil) != (other == nil) {
+		return false
+	}
+
+	if len(ds) != len(other) {
+		return false
+	}
+
+	for i, d := range ds {
+		if d != other[i] {
+			return false
+		}
+	}
 	return true
+}
+
+func (ds digits) add(other digits) digits {
+	var n int
+
+	if len(ds) < len(other) {
+		n = len(other)
+	} else {
+		n = len(ds)
+	}
+
+	var carry int8
+	// NOTE: As there may be a carry, set cap to n+1
+	result := make(digits, n, n+1)
+	for i := 0; i < n; i++ {
+		sum := carry
+		if i < len(ds) {
+			sum += ds[i]
+		}
+		if i < len(other) {
+			sum += other[i]
+		}
+		result[i] = sum % 10
+		carry = sum / 10
+	}
+
+	if carry > 0 {
+		result = append(result, carry)
+	}
+	return result
+}
+
+func (ds digits) number() string {
+	n := len(ds)
+	rs := make([]rune, n)
+	for i, d := range ds {
+		rs[n-1-i] = rune(d + '0')
+	}
+	return string(rs)
 }
--- karatsuba/digits_test.go
+++ karatsuba/digits_test.go
@@ -2,19 +2,91 @@ package karatsuba
 
 import "testing"
 
+func TestIsEqual(t *testing.T) {
+	tests := []struct {
+		a    digits
+		b    digits
+		want bool
+	}{
+		{nil, nil, true},
+		{nil, digits{}, false},
+		{digits{}, digits{}, true},
+		{digits{}, digits{1}, false},
+		{digits{1, 2, 3}, digits{1, 2, 3}, true},
+	}
+
+	for _, test := range tests {
+		ab := test.a.isEqual(test.b)
+		ba := test.b.isEqual(test.a)
+
+		if !(ab == ba && ba == test.want) {
+			t.Logf("%#v.isEqual(%#v) = %t", test.a, test.b, ab)
+			t.Logf("%#v.isEqual(%#v) = %t", test.b, test.a, ba)
+			t.Fail()
+		}
+	}
+}
+
 func TestToDigits(t *testing.T) {
 	tests := []struct {
 		input string
 		want  digits
-		err   error
+		ok    bool
 	}{
-		{"0", []int8{0}, nil},
+		{"", digits{}, true},
+		{"0", digits{0}, true},
+		{"12", digits{2, 1}, true},
+		{"a", nil, false},
 	}
 
 	for _, test := range tests {
 		got, err := toDigits(test.input)
-		if !isEqual(got, test.want) || err != test.err {
+		ok := err == nil
+		if !got.isEqual(test.want) || ok != test.ok {
 			t.Errorf("toDigits(%q) = %v, %v", test.input, got, err)
+		}
+	}
+}
+
+func TestNumber(t *testing.T) {
+	tests := []struct {
+		input digits
+		want  string
+	}{
+		{nil, ""},
+		{digits{0}, "0"},
+		{digits{1}, "1"},
+		{digits{2, 1}, "12"},
+	}
+
+	for _, test := range tests {
+		if got := test.input.number(); got != test.want {
+			t.Errorf("%#v.Number() = %q", test.input, got)
+		}
+	}
+}
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		a, b, want string
+	}{
+		{"0", "0", "0"},
+		{"1", "0", "1"},
+		{"1", "1", "2"},
+		{"1", "9", "10"},
+		{"10", "15", "25"},
+		{"22", "93", "115"},
+	}
+
+	for _, test := range tests {
+		a, _ := toDigits(test.a)
+		b, _ := toDigits(test.b)
+		ab := a.add(b).number()
+		ba := b.add(a).number()
+		if !(ab == ba && ba == test.want) {
+			t.Logf("%#v.add(%#v) = %q", test.a, test.b, ab)
+			t.Logf("%#v.add(%#v) = %q", test.b, test.a, ba)
+			t.Fail()
 		}
 	}
 }
